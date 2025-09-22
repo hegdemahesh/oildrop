167cca8347a1d1a9de724799f7328dad697dd2a4
Maheshchandra Hegde <hid.mahesh@gmail.com>
2025-09-22 11:18:02 +0530

    remove react native files

--- app/src/utils/excel.ts
+++
@@ -1,8 +0,0 @@
-import * as XLSX from 'xlsx';
-
-export function exportInvoicesToExcel(invoices: any[]) {
-  const ws = XLSX.utils.json_to_sheet(invoices);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Invoices');
-  return XLSX.write(wb, { type: 'base64', bookType: 'xlsx' });
-}
